9da196237e761ad52db7e0048fb71720510825eb
Julian <julianlx@hotmail.com>
2021-07-31 01:13:45 -0500

    adding calculator section

--- src/App.js
+++ src/App.js
@@ -1,72 +1,32 @@
 import './App.css';
 import Select from 'react-select';
 import Button from '@material-ui/core/Button';
-import ExchangeIcon from './Exchange_icon';
-
-const options = [
-  { value: 'BTC', label: 'BTC 0.1' },
-  { value: 'PVU', label: 'PVU 1.9' },
-  { value: 'BNB', label: 'BNB 5.0' }
-];
-
+import ExchangeIcon from './ExchangeIcon';
+import CurrencyRow from './CurrencyConverter';
+import FavCurrency from './FavCurrency';
+import Calculator from './Calculator';
 
 
 function App() {
-  const onDropdownChange = (value) => {
-    console.log(value);
-  }
-
   return (
     <>
       <div className="App">
-        <div style={{ width: '100%', margin: '20px auto', display: 'flex', justifyContent: 'center' }}>
-          <div style={{ width: '120px', margin: '0 20px' }}>
-            <Select
-              options={options}
-              onChange={onDropdownChange}
-            />
-          </div>
-          <div style={{ width: '120px', margin: '0 20px' }}>
-            <Select
-              options={options}
-              onChange={onDropdownChange}
-            />
-          </div>
-          <div style={{ width: '120px', margin: '0 20px' }}>
-            <Select
-              options={options}
-              onChange={onDropdownChange}
-            />
-          </div>
-          <div style={{ width: '120px', margin: '0 20px' }}>
-            <Select
-              options={options}
-              onChange={onDropdownChange}
-            />
-          </div>
-        </div>
-        <div style={{ width: '100%', margin: '20px auto', display: 'flex', justifyContent: 'center' }}>
-          <div style={{ width: '120px', margin: '0 20px' }}>
-            <Select
-              options={options}
-              onChange={onDropdownChange}
-            />
-          </div>
+        < FavCurrency />
 
+        <div style={{ width: '100%', margin: '20px auto', display: 'flex', justifyContent: 'center' }}>
+          <CurrencyRow />
           <ExchangeIcon />
+          <CurrencyRow />
 
-          <div style={{ width: '120px', margin: '0 20px' }}>
-            <Select
-              options={options}
-              onChange={onDropdownChange}
-            />
-          </div>
           <div style={{ margin: '0 20px' }}>
             <Button variant="contained" color="primary" style={{ width: '120px', height: '38px', margin: '0 20px' }}>
               USD
             </Button>
           </div>
         </div>
+
+        < Calculator />
+
       </div>
     </>
   );
