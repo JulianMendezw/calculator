81db32cadf3bb78b38fbf95a5d5a4cf31bc463fc
Julian <julianlx@hotmail.com>
2021-07-31 01:07:35 -0500

    adding favorite currency section

---
+++ src/FavCurrency.js
@@ -0,0 +1,46 @@
+import './App.css';
+import Select from 'react-select';
+import Button from '@material-ui/core/Button';
+import ExchangeIcon from './ExchangeIcon';
+import CurrencyRow from './CurrencyConverter';
+
+const options = [
+	{ value: 'BTC', label: 'BTC 0.1' },
+	{ value: 'PVU', label: 'PVU 1.9' },
+	{ value: 'BNB', label: 'BNB 5.0' }
+];
+
+
+export default function FavCurrency() {
+	const onDropdownChange = (value) => {
+		console.log(value);
+	}
+	return (
+		<div style={{ width: '100%', margin: '20px auto', display: 'flex', justifyContent: 'center' }}>
+			<div style={{ width: '120px', margin: '0 20px' }}>
+				<Select
+					options={options}
+					onChange={onDropdownChange}
+				/>
+			</div>
+			<div style={{ width: '120px', margin: '0 20px' }}>
+				<Select
+					options={options}
+					onChange={onDropdownChange}
+				/>
+			</div>
+			<div style={{ width: '120px', margin: '0 20px' }}>
+				<Select
+					options={options}
+					onChange={onDropdownChange}
+				/>
+			</div>
+			<div style={{ width: '120px', margin: '0 20px' }}>
+				<Select
+					options={options}
+					onChange={onDropdownChange}
+				/>
+			</div>
+		</div>
+	)
+}
